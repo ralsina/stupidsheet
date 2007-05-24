9334f8614c1dcca464fdb6b787bda06b2ff01732
roberto.alsina <roberto.alsina@a5fe96fe-2a31-0410-bca1-9102efe7537d>
2007-05-24 10:45:15 +0000

    The beginning of absolute and relative cells

--- trunk/ssheet/engine.py
+++ trunk/ssheet/engine.py
@@ -1,6 +1,6 @@
 import math
-import compiler
 import sys
+from traxcompiler import compile
 
 from PyQt4 import QtGui, QtCore
 
@@ -8,14 +8,19 @@ class SpreadSheet(QtCore.QObject):
     def __init__(self,parent):
         QtCore.QObject.__init__(self,parent)
     _cells = {}
+    _deps = {}
     tools = {}  
     def __setitem__(self, key, formula):
-        self._cells[key] = formula
+        key=key.lower()
+        c=compile('%s=%s;'%(key,formula))
+        self._cells[key] = c[key][0]
+        print c
         self.emit(QtCore.SIGNAL('changed'),(key))
-        print key
     def getformula(self, key):
-        return self._cells[key]
+        key=key.lower()
+        return self._cells[key][1]
     def __getitem__(self, key ):
+        print self._cells[key]
         return eval(self._cells[key], self.tools, self)
 
 def isKey(key):
