0ce94cba610c47a549055d1fee85f855f124193b
roberto.alsina <roberto.alsina@a5fe96fe-2a31-0410-bca1-9102efe7537d>
2007-05-28 22:54:11 +0000

    Fixed cellrange to use the new functions right

--- ssheet/engine.py
+++ ssheet/engine.py
@@ -55,34 +55,6 @@ class SpreadSheet(QtCore.QObject):
                 print 'evaluating [%s]: '%key,type(self._cells[key][0]),self._cells[key][0]
                 return eval(self._cells[key][0], self.tools, self)
 
-def isKey(key):
-    if (key[0].isalpha() and key[1:].isdigit()) or (key[0:1].isalpha() and key[2:].isdigit()):
-        return True
-    return False
-
-def coordKey(x,y):
-    if x< 26:
-        key=chr(97+x)
-    else:
-        key=chr(97+int(x/26))+chr(97+x%26)
-    key=key+str(y+1)
-    return key
-    
-    
-def keyCoord(key):      
-    if key[1].isalpha():
-        x=(ord(key[0])-97)*26+ord(key[1])-97
-        y=int(key[2:])-1
-    else:
-        x=ord(key[0])-97
-        y=int(key[1:])-1
-    return (x,y)
-
-try:
-    import psyco
-    psyco.bind(SpreadSheet.__getitem__)
-except:
-    pass
 
         
 if __name__ == "__main__":  
