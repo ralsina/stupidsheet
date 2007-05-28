269feb354cad24d4a071b13a8abb6c0986293094
roberto.alsina <roberto.alsina@a5fe96fe-2a31-0410-bca1-9102efe7537d>
2007-05-28 22:50:35 +0000

    Removed redundant functions.

--- ssheet/cellutils.py
+++ ssheet/cellutils.py
@@ -1,6 +1,28 @@
 from pprint import pprint
 import aperiot.lexer as lexer
 
+def isKey(key):
+    if (key[0].isalpha() and key[1:].isdigit()) or (key[0:1].isalpha() and key[2:].isdigit()):
+        return True
+    return False
+
+def coordKey(x,y):
+    if x< 26:
+        key=chr(97+x)
+    else:
+        key=chr(97+int(x/26))+chr(97+x%26)
+    key=key+str(y+1)
+    return key
+    
+    
+def keyCoord(key):      
+    if key[1].isalpha():
+        x=(ord(key[0])-97)*26+ord(key[1])-97
+        y=int(key[2:])-1
+    else:
+        x=ord(key[0])-97
+        y=int(key[1:])-1
+    return (x,y)
 
 def splitcell(cname):
     # Sure, this only works for well-formed cellnames ;-)
@@ -13,30 +35,13 @@ def splitcell(cname):
             row+=c
     return col,int(row)
 
-def col2num(col):
-    ac=0
-    a=ord('a')
-    col=col.lower()
-    i=1
-    for c in col:
-        v=ord(c)-a+1
-        ac+=v*(26**(len(col)-i))
-        i+=1
-    return ac
-
-def num2col(num):
-    res=chr(65+(num-1)%26).lower()
-    if num > 26:
-        res=chr(64+(num-1)/26).lower()+res
-    return res
-
 def cellrange(start,end):
     sc,sr=splitcell(start)
     ec,er=splitcell(end)
 
     res=[]
-    for col in xrange(col2num(sc),col2num(ec)+1):
+    for col in xrange(keyCoord(sc),col2num(ec)+1):
         for row in xrange(sr,er+1):
-                res.append(lexer.Identifier(num2col(col)+str(row)))
+                res.append(lexer.Identifier(coordKey(col)+str(row)))
 
     return res
