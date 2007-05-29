f2b8d474ab0c2a36c61baa081a9a4a5a2bfe963a
roberto.alsina <roberto.alsina@a5fe96fe-2a31-0410-bca1-9102efe7537d>
2007-05-29 14:15:09 +0000

    Give the compiler a class interface

--- trunk/ssheet/traxcompiler.py
+++ trunk/ssheet/traxcompiler.py
@@ -1,33 +1,30 @@
 from pprint import pprint
-from aperiot.parsergen import build_parser
+from aperiot.parsergen import load_parser
 import aperiot
 from cellutils import *
 import sys
 
-dependencies=set()
-currentKey=None
-
-def addOp(*args):
-        return '+'.join([compile_token(a) for a in args])
-def mulOp(*args):
-        return '*'.join([compile_token(a) for a in args])
-def subOp(*args):
-        return '-'.join([compile_token(a) for a in args])
-def divOp(*args):
-        return '/'.join([compile_token(a) for a in args])
-def groupOp(*args):
-        return '(%s)'%compile_token(args[0])
-def funcOp(*args):
+def addOp(self,*args):
+        return '+'.join([self.compile_token(a) for a in args])
+def mulOp(self,*args):
+        return '*'.join([self.compile_token(a) for a in args])
+def subOp(self,*args):
+        return '-'.join([self.compile_token(a) for a in args])
+def divOp(self,*args):
+        return '/'.join([self.compile_token(a) for a in args])
+def groupOp(self,*args):
+        return '(%s)'%self.compile_token(args[0])
+def funcOp(self,*args):
         return '%s(%s)'%(args[0],
-                         ','.join([compile_token(a) for a in args[1]]))
+                         ','.join([self.compile_token(a) for a in args[1]]))
 
-def rangeOp(*args):
-        c1=compile_token(args[0])
-        c2=compile_token(args[1])
-        return ','.join([compile_token(a) for a in cellrange(c1,c2)])
-def cellOp(*args):
-        return ''.join([compile_token(a) for a in args])
-def elemOp(*args):
+def rangeOp(self,*args):
+        c1=self.compile_token(args[0])
+        c2=self.compile_token(args[1])
+        return ','.join([self.compile_token(a) for a in cellrange(c1,c2)])
+def cellOp(self,*args):
+        return ''.join([self.compile_token(a) for a in args])
+def elemOp(self,*args):
         return str(args[0])
 
 
@@ -45,31 +42,9 @@ operators={'+':addOp,
            'absrow':elemOp
            }
 
-def compile_token(token):
-        if isinstance (token,aperiot.lexer.Identifier):
-            dependencies.add(token.symbolic_name.lower())
-            return token.symbolic_name.lower()
-        if isinstance(token,list):
-            return apply(operators[token[0]],token[1:])
-        return str(token)
-
-def compile_assignment(tokens):
-        global currentKey
-        currentKey=tokens[0].symbolic_name
-        compiled=compile_token(tokens[1])
-        return currentKey,compiled
 
 def traxcompile(source):
     global dependencies
-    compiled={}
-    traxparser = build_parser('traxter')
-    assign_list=traxparser.parse(source)
-    for assignment in assign_list:
-            pprint(assignment)
-            dependencies=set()
-            var,c=compile_assignment(assignment)
-            compiled[var]=[c,dependencies]
-    return compiled
 
 def traverse_tree(tokens,func,extra_args):
         '''applies func to all tokens in a tree'''
@@ -78,22 +53,49 @@ def traverse_tree(tokens,func,extra_args):
                 if type(token)==list:
                         traverse_tree(token,func,extra_args)
 
+class Compiler:
+        def __init__(self):
+                self.dependencies=set()
+                self.parser=load_parser('traxter',verbose=True)
+        def compile(self,source):
+            compiled={}
+            assign_list=self.parser.parse(source)
+            for assignment in assign_list:
+                    pprint(assignment)
+                    self.dependencies=set()
+                    var,c=self.compile_assignment(assignment)
+                    compiled[var]=[c,self.dependencies]
+            return compiled
+
+        def compile_token(self,token):
+                if isinstance (token,aperiot.lexer.Identifier):
+                    self.dependencies.add(token.symbolic_name.lower())
+                    return token.symbolic_name.lower()
+                if isinstance(token,list):
+                    return apply(operators[token[0]],[self]+token[1:])
+                return str(token)
+
+        def compile_assignment(self,tokens):
+                currentKey=tokens[0].symbolic_name
+                compiled=self.compile_token(tokens[1])
+                return currentKey,compiled
 
 if __name__=="__main__":
+        c=Compiler()
         t='A1=SUM(A1:A7, C3:C9);'
-        pprint (traxcompile(t))
+        pprint (c.compile(t))
         print
         print
         t='A1=SUM(A1:A7);'
-        pprint (traxcompile(t))
+        pprint (c.compile(t))
         print
         print
         t='A1=SUM(AVG(A1:A7))*2;'
-        pprint (traxcompile(t))
+        pprint (c.compile(t))
         print
         print
         t='A1=$A1+A$1+$A$1+A1;'
-        pprint (traxcompile(t))
+        pprint (c.compile(t))
 
 
 
