87812d68332c5075056957516096136b8adbcfd6
roberto.alsina <roberto.alsina@a5fe96fe-2a31-0410-bca1-9102efe7537d>
2007-05-30 14:39:23 +0000

    Bored of doing it manually

---
+++ compile_resources.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Regenerating Traxter parser"
+apr -f -d StupidSheet/compiler/ StupidSheet/compiler/traxter.apr
+
+echo "Compiling UI files"
+for ui in StupidSheet/ui/*.ui
+do
+  pyuic4 $ui -o `dirname $ui`/Ui_`basename $ui .ui`.py
+done
+echo "Compiling resource files"
+for qrc in StupidSheet/ui/*.qrc
+do
+  pyrcc4 $qrc -o `dirname $ui`/`basename $ui .qrc`_rc.py
+done
